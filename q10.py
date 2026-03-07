5caaea8ac585eff623dc3857f9590bc610074855
Akshat Mishra <akshat936942@gmail.com>
2026-03-07 14:45:50 +0530

    daikly

---
+++ q10.py
@@ -0,0 +1,5 @@
+#Write a program to print numbers from 1 to n.
+num =int(input("enter a number"))
+for  i in (num):
+    print(i)
+    
---
+++ q9.py
@@ -0,0 +1,1 @@
+#Write a program to print even numbers from 1 to 50.
